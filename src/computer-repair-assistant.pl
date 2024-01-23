ac8f4033d5748dc1ad9f8e8dd50e3a7c320cd01c
Antonio Robirosa <19760446+arobirosa@users.noreply.github.com>
2024-01-23 09:12:34 +0200

    Rename of the variables

--- src/computer-repair-assistant.pl
+++ src/computer-repair-assistant.pl
@@ -2,10 +2,9 @@
 % It proposes the possible cause of the malfunction by asking you the observed symptoms.
 %
 % Predicates
-%   start
-%   restart
-%   listsymptoms
-%
+%   start   Starts a new repair case. It removes all old facts entered by the user.
+%   report  Generates a report
+%   change  Modifies the answers
 
 % Definition of symptoms
 
@@ -14,37 +13,35 @@
 % Internal predicates of the expert system. Do not touch.
 
 
-
-
 go :-
-write('What is the patient''s name? '),
-read(Patient),get_single_char(Code),
-hypothesis(Patient,Disease),
-write_list([Patient,', probably has ',Disease,'.']),nl.
+write('What is repair case ID? '),
+read(RepairCase),get_single_char(Code),
+brokenComponent(RepairCase,Disease),
+write_list([RepairCase,', probably has ',Disease,'.']),nl.
 
 go :-
 write('Sorry, I don''t seem to be able to'),nl,
 write('diagnose the disease.'),nl.
 
-symptom(Patient,fever) :-  verify(Patient," have a fever (y/n) ?").
-symptom(Patient,rash) :- verify(Patient," have a rash (y/n) ?").
-symptom(Patient,headache) :- verify(Patient," have a headache (y/n) ?").
-symptom(Patient,runny_nose) :-  verify(Patient," have a runny_nose (y/n) ?").
-symptom(Patient,conjunctivitis) :- verify(Patient," have a conjunctivitis (y/n) ?").
-symptom(Patient,cough) :- verify(Patient," have a cough (y/n) ?").
-symptom(Patient,body_ache) :-
-verify(Patient," have a body_ache (y/n) ?").
-symptom(Patient,chills) :-
-verify(Patient," have a chills (y/n) ?").
-symptom(Patient,sore_throat) :-
-verify(Patient," have a sore_throat (y/n) ?").
-symptom(Patient,sneezing) :-
-verify(Patient," have a sneezing (y/n) ?").
-symptom(Patient,swollen_glands) :-
-verify(Patient," have a swollen_glands (y/n) ?").
+symptom(RepairCase,fever) :-  verify(RepairCase," have a fever (y/n) ?").
+symptom(RepairCase,rash) :- verify(RepairCase," have a rash (y/n) ?").
+symptom(RepairCase,headache) :- verify(RepairCase," have a headache (y/n) ?").
+symptom(RepairCase,runny_nose) :-  verify(RepairCase," have a runny_nose (y/n) ?").
+symptom(RepairCase,conjunctivitis) :- verify(RepairCase," have a conjunctivitis (y/n) ?").
+symptom(RepairCase,cough) :- verify(RepairCase," have a cough (y/n) ?").
+symptom(RepairCase,body_ache) :-
+verify(RepairCase," have a body_ache (y/n) ?").
+symptom(RepairCase,chills) :-
+verify(RepairCase," have a chills (y/n) ?").
+symptom(RepairCase,sore_throat) :-
+verify(RepairCase," have a sore_throat (y/n) ?").
+symptom(RepairCase,sneezing) :-
+verify(RepairCase," have a sneezing (y/n) ?").
+symptom(RepairCase,swollen_glands) :-
+verify(RepairCase," have a swollen_glands (y/n) ?").
 
-ask(Patient,Question) :-
-	write(Patient),write(', do you'),write(Question),
+ask(RepairCase,Question) :-
+	write(RepairCase),write(', do you'),write(Question),
 	read(N),
 	( (N == yes ; N == y)
       ->
@@ -63,45 +60,45 @@ undo :- retract(no(_)),fail.
 undo.
 
 
-hypothesis(Patient,german_measles) :-
-symptom(Patient,fever),
-symptom(Patient,headache),
-symptom(Patient,runny_nose),
-symptom(Patient,rash).
+brokenComponent(RepairCase,german_measles) :-
+symptom(RepairCase,fever),
+symptom(RepairCase,headache),
+symptom(RepairCase,runny_nose),
+symptom(RepairCase,rash).
 
-hypothesis(Patient,common_cold) :-
-symptom(Patient,headache),
-symptom(Patient,sneezing),
-symptom(Patient,sore_throat),
-symptom(Patient,runny_nose),
-symptom(Patient,chills).
+brokenComponent(RepairCase,common_cold) :-
+symptom(RepairCase,headache),
+symptom(RepairCase,sneezing),
+symptom(RepairCase,sore_throat),
+symptom(RepairCase,runny_nose),
+symptom(RepairCase,chills).
 
-hypothesis(Patient,measles) :-
-symptom(Patient,cough),
-symptom(Patient,sneezing),
-symptom(Patient,runny_nose).
+brokenComponent(RepairCase,measles) :-
+symptom(RepairCase,cough),
+symptom(RepairCase,sneezing),
+symptom(RepairCase,runny_nose).
 
-hypothesis(Patient,flu) :-
-symptom(Patient,fever),
-symptom(Patient,headache),
-symptom(Patient,body_ache),
-symptom(Patient,conjunctivitis),
-symptom(Patient,chills),
-symptom(Patient,sore_throat),
-symptom(Patient,runny_nose),
-symptom(Patient,cough).
+brokenComponent(RepairCase,flu) :-
+symptom(RepairCase,fever),
+symptom(RepairCase,headache),
+symptom(RepairCase,body_ache),
+symptom(RepairCase,conjunctivitis),
+symptom(RepairCase,chills),
+symptom(RepairCase,sore_throat),
+symptom(RepairCase,runny_nose),
+symptom(RepairCase,cough).
 
 
 
-hypothesis(Patient,mumps) :-
-symptom(Patient,fever),
-symptom(Patient,swollen_glands).
+brokenComponent(RepairCase,mumps) :-
+symptom(RepairCase,fever),
+symptom(RepairCase,swollen_glands).
 
-hypothesis(Patient,chicken_pox) :-
-symptom(Patient,fever),
-symptom(Patient,chills),
-symptom(Patient,body_ache),
-symptom(Patient,rash).
+brokenComponent(RepairCase,chicken_pox) :-
+symptom(RepairCase,fever),
+symptom(RepairCase,chills),
+symptom(RepairCase,body_ache),
+symptom(RepairCase,rash).
 
 write_list([]).
 write_list([Term| Terms]) :-
