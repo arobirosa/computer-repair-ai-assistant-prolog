f4cc930c1aef7920b3d65f5fb5e7f1fc59a89508
Antonio Robirosa <19760446+arobirosa@users.noreply.github.com>
2024-01-23 09:50:22 +0200

    Now it is clear what predicates can be changed by the user

--- src/computer-repair-assistant.pl
+++ src/computer-repair-assistant.pl
@@ -6,22 +6,7 @@
 %   report  Generates a report
 %   change  Modifies the answers
 
-% Definition of symptoms
-
-% Definition of repair causes
-
-% Internal predicates of the expert system. Do not touch.
-
-
-go :-
-write('What is repair case ID? '),
-read_line_to_string(user_input, RepairCase),
-brokenComponent(Disease),
-write_list([RepairCase,', probably has ',Disease,'.']),nl.
-
-go :-
-write('Sorry, I don''t seem to be able to'),nl,
-write('diagnose the disease.'),nl.
+%%%%%%% DEFINITION OF SYMPTOMS
 
 symptom(fever) :-  verify(" have a fever?").
 symptom(rash) :- verify(" have a rash?").
@@ -35,7 +20,61 @@ symptom(sore_throat) :- verify(" have a sore_throat?").
 symptom(sneezing) :- verify(" have a sneezing?").
 symptom(swollen_glands) :- verify(" have a swollen_glands?").
 
+%%%%%%% DEFINITION OF BROKEN COMPONENT
+
+brokenComponent(german_measles) :-
+    symptom(fever),
+    symptom(headache),
+    symptom(runny_nose),
+    symptom(rash).
+
+brokenComponent(common_cold) :-
+    symptom(headache),
+    symptom(sneezing),
+    symptom(sore_throat),
+    symptom(runny_nose),
+    symptom(chills).
+
+brokenComponent(measles) :-
+    symptom(cough),
+    symptom(sneezing),
+    symptom(runny_nose).
+
+brokenComponent(flu) :-
+    symptom(fever),
+    symptom(headache),
+    symptom(body_ache),
+    symptom(conjunctivitis),
+    symptom(chills),
+    symptom(sore_throat),
+    symptom(runny_nose),
+    symptom(cough).
+
+brokenComponent(mumps) :-
+    symptom(fever),
+    symptom(swollen_glands).
+
+brokenComponent(chicken_pox) :-
+    symptom(fever),
+    symptom(chills),
+    symptom(body_ache),
+    symptom(rash).
+
+
+%%%%%% INTERNAL PREDICATES OF THE EXPERT SYSTEM. DO NOT TOUCH.
+
+% Informs the engine that these predicates will change during execution. They are inputted by the user.
 :- dynamic yes/1,no/1.
+
+go :-
+    write('What is repair case ID? '),
+    read_line_to_string(user_input, RepairCase),
+    brokenComponent(Disease),
+    write_list(['Repair case ', RepairCase,': The broken component probably is ',Disease,'.']),nl.
+
+go :-
+    write('Sorry, I don''t seem to be able to'),nl,
+    write('diagnose the broken component. Please contact a technician.'),nl.
 
 verify(S) :-
    (yes(S) -> true ;
@@ -46,58 +85,14 @@ undo :- retract(yes(_)),fail.
 undo :- retract(no(_)),fail.
 undo.
 
+start :- undo, go.
 
-brokenComponent(german_measles) :-
-symptom(fever),
-symptom(headache),
-symptom(runny_nose),
-symptom(rash).
+%%%%%%% Other predicates %%%%%%%%%
 
-brokenComponent(common_cold) :-
-symptom(headache),
-symptom(sneezing),
-symptom(sore_throat),
-symptom(runny_nose),
-symptom(chills).
-
-brokenComponent(measles) :-
-symptom(cough),
-symptom(sneezing),
-symptom(runny_nose).
-
-brokenComponent(flu) :-
-symptom(fever),
-symptom(headache),
-symptom(body_ache),
-symptom(conjunctivitis),
-symptom(chills),
-symptom(sore_throat),
-symptom(runny_nose),
-symptom(cough).
-
-
-
-brokenComponent(mumps) :-
-symptom(fever),
-symptom(swollen_glands).
-
-brokenComponent(chicken_pox) :-
-symptom(fever),
-symptom(chills),
-symptom(body_ache),
-symptom(rash).
-
+% Prints the elements of the list
 write_list([]).
-write_list([Term| Terms]) :-
-write(Term),
+write_list([Term| Terms]) :- write(Term),
 write_list(Terms).
-
-response(Reply) :-
-get_single_char(Code),
-put_code(Code), nl,
-char_code(Reply, Code).
-
-% Other predicates
 
 % Predicate to read a yes/no answer
 ask(Question) :-
