acdc0b748c13ad7cf925ab5005a0e0576597a730
Antonio Robirosa <19760446+arobirosa@users.noreply.github.com>
2024-01-30 10:15:15 +0100

    Now the German language is supported. Correction of the translations

--- src/translations_de.pl
+++ src/translations_de.pl
@@ -5,16 +5,16 @@
         prolog:message//1.
 
 prolog:message(battery_light_turns_on(de)) --> ["Schaltet sich das Batterielicht ein, nachdem der Netzschalter eingeschaltet wurde?"].
-prolog:message(hard_disk_spins_on_a_second_motherboard(en)) --> ["Läuf die Festplatte, wenn sie an ein zweites Motherboard angeschlossen ist?"].
+prolog:message(hard_disk_spins_on_a_second_motherboard(de)) --> ["Läuf die Festplatte, wenn sie an ein zweites Motherboard angeschlossen ist?"].
 prolog:message(known_working_hard_disk_do_not_spin_on_motherboard(de)) --> ["Läuf eine Festplatte, von der bekannt ist, dass sie funktioniert, nicht auf dem Motherboard?"].
-prolog:message(ram_modules_not_compatible_with_motherboard(en)) --> ["Die Geschwindigkeit und Typ der RAM-Module sind nicht im Handbuch des Motherboards dokumentiert?"].
-prolog:message(ram_modules_were_updated_lately(en)) --> ["Wurde das RAM-Modul in letzter Zeit ausgetauscht?"].
+prolog:message(ram_modules_not_compatible_with_motherboard(de)) --> ["Die Geschwindigkeit und Typ der RAM-Module sind nicht im Handbuch des Motherboards dokumentiert?"].
+prolog:message(ram_modules_were_updated_lately(de)) --> ["Wurde das RAM-Modul in letzter Zeit ausgetauscht?"].
 prolog:message(screen_is_not_displaying_anything(de)) --> ["Ist der Bildschirm leer? Er zeigt nichts an"].
 prolog:message(system_do_not_turn_on(de)) --> ["Das System schaltet sich nicht ein, richtig?"].
 prolog:message(system_turns_on(de)) --> ["Schaltet sich das System ein?"].
-prolog:message(system_do_not_work_after_reinserting_power_cable_connector(en)) --> ["Das System schaltet sich nicht ein, nachdem der Stromanschluss vom Netzteil zum Motherboard herausgenommen und wieder eingesteckt wurde, richtig?"].
-prolog:message(system_works_after_reinserting_power_cable_connector(en)) --> ["Das System schaltet sich ein, nachdem der Stromstecker vom Netzteil zum Motherboard herausgenommen und wieder eingesteckt wurde, richtig?"].
-prolog:message(system_do_not_work_with_another_power_cord(en)) --> ["Das System schaltet sich nicht mit einem anderen Netzkabel ein, richtig?"].
+prolog:message(system_do_not_work_after_reinserting_power_cable_connector(de)) --> ["Das System schaltet sich nicht ein, nachdem der Stromanschluss vom Netzteil zum Motherboard herausgenommen und wieder eingesteckt wurde, richtig?"].
+prolog:message(system_works_after_reinserting_power_cable_connector(de)) --> ["Das System schaltet sich ein, nachdem der Stromstecker vom Netzteil zum Motherboard herausgenommen und wieder eingesteckt wurde, richtig?"].
+prolog:message(system_do_not_work_with_another_power_cord(de)) --> ["Das System schaltet sich nicht mit einem anderen Netzkabel ein, richtig?"].
 prolog:message(system_has_a_power_supply_unit(de)) --> ["Verfügt das System über ein Netzteil? Laptops haben keins, aber Desktops schon."].
 prolog:message(system_works_after_reinserting_the_ram_modules(de)) --> ["Nachdem Sie das/die RAM-Modul(e) entfernt" ,
     " entfernt und in den Steckplatz eingesetzt haben, funktioniert das System?"].
@@ -22,7 +22,7 @@ prolog:message(system_works_on_another_outlet(de)) --> ["Funktioniert das System
 prolog:message(system_works_with_a_new_motherboard(de)) --> ["Funktioniert der Computer mit einem neuen Motherboard?"].
 prolog:message(system_works_with_a_new_power_supply_unit(de)) --> ["Funktioniert der Computer mit einem neuen Netzteil?"].
 prolog:message(system_works_with_another_power_cord(de)) --> ["Funktioniert der Computer mit einem anderen Netzkabel?"].
-prolog:message(system_works_with_old_modules(de)) --> ["Funktioniert der Computer mit dem/den alten RAM-Modul(en)?"].
+prolog:message(system_works_with_old_modules(de)) --> ["Funktioniert der Computer mit dem/den alten RAM-Modul?"].
 
 prolog:message(no_electricity_on_the_power_outlet(de)) --> ["Kein Strom an der Steckdose"].
 prolog:message(power_cord_is_broken(de)) --> ["Das Netzkabel ist kaputt"].
@@ -32,11 +32,11 @@ prolog:message(power_supply_unit_is_burnt(de)) --> ["Das Netzteil ist verbrannt"
 prolog:message(incompatible_ram_modules(de)) --> ["Das RAM-Modul ist nicht mit der Hauptplatine kompatibel"].
 prolog:message(disconnected_ram_modules(de)) --> ["Das RAM-Modul saß falsch auf dem Memory Slot"].
 
-prolog:message(questions_answered_yes(en)) --> ["=== Mit ja beantwortete Fragen === ", nl].
-prolog:message(questions_answered_no(en)) --> ["=== Mit nein beantwortete Fragen === ", nl].
-prolog:message(questions_answered_report_end(en)) --> ["=== Ende des Berichts ===", nl].
+prolog:message(questions_answered_yes(de)) --> ["=== Mit ja beantwortete Fragen === ", nl].
+prolog:message(questions_answered_no(de)) --> ["=== Mit nein beantwortete Fragen === ", nl].
+prolog:message(questions_answered_report_end(de)) --> ["=== Ende des Berichts ===", nl].
 
 prolog:message(answer_keys_list(de)) --> [" (yes/y/no/n) "]. % TODO Translate the keys
-prolog:message(diagnose_is(en)) --> ["Die Diagnose ist: "].
-prolog:message(unable_to_diagnose_broken_component(en)) --> ["Ich kann nicht herausfinden, welche Komponente kaputt ist. ",
+prolog:message(diagnose_is(de)) --> ["Die Diagnose ist: "].
+prolog:message(unable_to_diagnose_broken_component(de)) --> ["Ich kann nicht herausfinden, welche Komponente kaputt ist. ",
     "Bitte wenden Sie sich an einen Techniker.", nl].
--- src/translations_es.pl
+++ src/translations_es.pl
@@ -10,7 +10,7 @@ prolog:message(known_working_hard_disk_do_not_spin_on_motherboard(es)) --> ["¿U
 prolog:message(ram_modules_not_compatible_with_motherboard(es)) --> ["¿La velocidad y la forma de los módulos RAM no están documentadas en el manual de la placa base?"].
 prolog:message(ram_modules_were_updated_lately(es)) --> ["¿Se ha cambiado el módulo RAM recientemente?"].
 prolog:message(screen_is_not_displaying_anything(es)) --> ["¿La pantalla está en blanco? No muestra nada"].
-prolog:message(sistema_no_enciende(es)) --> ["El sistema no se enciende, ¿es cierto?"].
+prolog:message(system_do_not_turn_on(es)) --> ["El sistema no se enciende, ¿es cierto?"].
 prolog:message(system_turns_on(es)) --> ["¿Se enciende el sistema?"].
 prolog:message(system_do_not_work_after_reinserting_power_cable_connector(es)) --> ["El sistema no se enciende después de sacar y volver a insertar el conector de alimentación de la fuente de alimentación a la placa base, ¿verdad?"].
 prolog:message(system_works_after_reinserting_power_cable_connector(es)) --> ["El sistema se enciende después de sacar y volver a insertar el conector de alimentación de la PSU a la placa base, ¿verdad?"].
@@ -25,7 +25,7 @@ prolog:message(system_works_with_a_new_power_supply_unit(es)) --> ["¿Funciona e
 prolog:message(system_works_with_another_power_cord(es)) --> ["¿Funciona el ordenador con otro cable de alimentación?"].
 prolog:message(system_works_with_old_modules(es)) --> ["¿Funciona el ordenador con los módulos RAM antiguos?"].
 
-prolog:message(no_electricidad_en_la_toma_de_corriente(es)) --> ["No hay electricidad en el tomacorriente"].
+prolog:message(no_electricity_on_the_power_outlet(es)) --> ["No hay electricidad en el tomacorriente"].
 prolog:message(power_cord_is_broken(es)) --> ["El cable de alimentación está roto"].
 prolog:message(motherboard_power_cable_is_disconnected(es)) --> ["El cable que da electricidad a la placa base se ha desconectado"].
 prolog:message(motherboard_is_burnt(es)) --> ["La placa base está quemada"].
@@ -33,7 +33,7 @@ prolog:message(power_supply_unit_is_burnt(es)) --> ["La fuente de alimentación 
 prolog:message(incompatible_ram_modules(es)) --> ["El módulo RAM es incompatible con la placa base"].
 prolog:message(disconnected_ram_modules(es)) --> ["El módulo RAM se ha desconectado de la placa base"].
 
-prolog:message(questions_answered_yes(en)) --> ["=== Preguntas contestadas con sí === ", nl].
+prolog:message(questions_answered_yes(es)) --> ["=== Preguntas contestadas con sí === ", nl].
 prolog:message(questions_answered_no(es)) --> ["=== Preguntas contestadas con no === ", nl].
 prolog:message(questions_answered_report_end(es)) --> ["=== Fin del informe ===", nl].
 
