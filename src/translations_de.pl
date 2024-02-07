74ac62ed3c60d37011c18f48e5795c1edbe500a0
Antonio Robirosa <19760446+arobirosa@users.noreply.github.com>
2024-02-07 12:32:58 +0100

    New the assistent has a web interface. Not working

--- src/translations_de.pl
+++ src/translations_de.pl
@@ -15,40 +15,43 @@
 %
 
 % This file contain all the translations in German from Germany.
+:- module(translations_de,
+	  [ message_translation//2]).
 
 :- multifile
-          prolog:message//1.
+        prolog:message//1, message_translation//2.
 
-prolog:message(battery_light_turns_on(de)) --> ["Schaltet sich das Batterielicht ein, nachdem der Netzschalter eingeschaltet wurde?"].
-prolog:message(hard_disk_spins_on_a_second_motherboard(de)) --> ["Läuf die Festplatte, wenn sie an ein zweites Motherboard angeschlossen ist?"].
-prolog:message(known_working_hard_disk_do_not_spin_on_motherboard(de)) --> ["Läuf eine Festplatte, von der bekannt ist, dass sie funktioniert, nicht auf dem Motherboard?"].
-prolog:message(ram_modules_not_compatible_with_motherboard(de)) --> ["Die Geschwindigkeit und Typ der RAM-Module sind nicht im Handbuch des Motherboards dokumentiert?"].
-prolog:message(ram_modules_were_updated_lately(de)) --> ["Wurde das RAM-Modul in letzter Zeit ausgetauscht?"].
-prolog:message(screen_is_not_displaying_anything(de)) --> ["Ist der Bildschirm leer? Er zeigt nichts an"].
-prolog:message(system_turns_on(de)) --> ["Schaltet sich das System ein?"].
-prolog:message(system_works_after_reinserting_power_cable_connector(de)) --> ["Das System schaltet sich ein, nachdem der Stromstecker vom Netzteil zum Motherboard herausgenommen und wieder eingesteckt wurde, richtig?"].
-prolog:message(system_has_a_power_supply_unit(de)) --> ["Verfügt das System über ein Netzteil? Laptops haben keins, aber Desktops schon."].
-prolog:message(system_works_after_reinserting_the_ram_modules(de)) --> ["Nachdem Sie das RAM-Modul entfernt" ,
-    " und in den Steckplatz eingesetzt haben, funktioniert das System?"].
-prolog:message(system_works_on_another_outlet(de)) --> ["Funktioniert das System an einer anderen Steckdose?"].
-prolog:message(system_works_with_a_new_motherboard(de)) --> ["Funktioniert der Computer mit einem neuen Motherboard?"].
-prolog:message(system_works_with_a_new_power_supply_unit(de)) --> ["Funktioniert der Computer mit einem neuen Netzteil?"].
-prolog:message(system_works_with_another_power_cord(de)) --> ["Funktioniert der Computer mit einem anderen Netzkabel?"].
-prolog:message(system_works_with_old_modules(de)) --> ["Funktioniert der Computer mit dem/den alten RAM-Modul?"].
+:- discontiguous message_translation/2.
 
-prolog:message(no_electricity_on_the_power_outlet(de)) --> ["Kein Strom an der Steckdose"].
-prolog:message(power_cord_is_broken(de)) --> ["Das Netzkabel ist kaputt"].
-prolog:message(motherboard_power_cable_is_disconnected(de)) --> ["Das Kabel, das die Motherboard mit Strom versorgt, wurde abgezogen"].
-prolog:message(motherboard_is_burnt(de)) --> ["Die Motherboard ist verbrannt"].
-prolog:message(power_supply_unit_is_burnt(de)) --> ["Das Netzteil ist verbrannt"].
-prolog:message(incompatible_ram_modules(de)) --> ["Das RAM-Modul ist nicht mit der Hauptplatine kompatibel"].
-prolog:message(disconnected_ram_modules(de)) --> ["Das RAM-Modul saß falsch auf dem Memory Slot"].
+message_translation(battery_light_turns_on(de),["Schaltet sich das Batterielicht ein, nachdem der Netzschalter eingeschaltet wurde?"]).
+message_translation(hard_disk_spins_on_a_second_motherboard(de),["Läuf die Festplatte, wenn sie an ein zweites Motherboard angeschlossen ist?"]).
+message_translation(known_working_hard_disk_do_not_spin_on_motherboard(de),["Läuf eine Festplatte, von der bekannt ist, dass sie funktioniert, nicht auf dem Motherboard?"]).
+message_translation(ram_modules_not_compatible_with_motherboard(de),["Die Geschwindigkeit und Typ der RAM-Module sind nicht im Handbuch des Motherboards dokumentiert?"]).
+message_translation(ram_modules_were_updated_lately(de),["Wurde das RAM-Modul in letzter Zeit ausgetauscht?"]).
+message_translation(screen_is_not_displaying_anything(de),["Ist der Bildschirm leer? Er zeigt nichts an"]).
+message_translation(system_turns_on(de),["Schaltet sich das System ein?"]).
+message_translation(system_works_after_reinserting_power_cable_connector(de),["Das System schaltet sich ein, nachdem der Stromstecker vom Netzteil zum Motherboard herausgenommen und wieder eingesteckt wurde, richtig?"]).
+message_translation(system_has_a_power_supply_unit(de),["Verfügt das System über ein Netzteil? Laptops haben keins, aber Desktops schon."]).
+message_translation(system_works_after_reinserting_the_ram_modules(de),["Nachdem Sie das RAM-Modul entfernt" ,
+    " und in den Steckplatz eingesetzt haben, funktioniert das System?"]).
+message_translation(system_works_on_another_outlet(de),["Funktioniert das System an einer anderen Steckdose?"]).
+message_translation(system_works_with_a_new_motherboard(de),["Funktioniert der Computer mit einem neuen Motherboard?"]).
+message_translation(system_works_with_a_new_power_supply_unit(de),["Funktioniert der Computer mit einem neuen Netzteil?"]).
+message_translation(system_works_with_another_power_cord(de),["Funktioniert der Computer mit einem anderen Netzkabel?"]).
+message_translation(system_works_with_old_modules(de),["Funktioniert der Computer mit dem/den alten RAM-Modul?"]).
+
+message_translation(no_electricity_on_the_power_outlet(de),["Kein Strom an der Steckdose"]).
+message_translation(power_cord_is_broken(de),["Das Netzkabel ist kaputt"]).
+message_translation(motherboard_power_cable_is_disconnected(de),["Das Kabel, das die Motherboard mit Strom versorgt, wurde abgezogen"]).
+message_translation(motherboard_is_burnt(de),["Die Motherboard ist verbrannt"]).
+message_translation(power_supply_unit_is_burnt(de),["Das Netzteil ist verbrannt"]).
+message_translation(incompatible_ram_modules(de),["Das RAM-Modul ist nicht mit der Hauptplatine kompatibel"]).
+message_translation(disconnected_ram_modules(de),["Das RAM-Modul saß falsch auf dem Memory Slot"]).
 
 prolog:message(questions_answered_yes(de)) --> ["=== Mit ja beantwortete Fragen === ", nl].
 prolog:message(questions_answered_no(de)) --> ["=== Mit nein beantwortete Fragen === ", nl].
 prolog:message(questions_answered_report_end(de)) --> ["=== Ende des Berichts ===", nl].
 
-prolog:message(answer_keys_list(de)) --> [" (ja/j/nein/n) "].
-prolog:message(diagnose_is(de)) --> ["Die Diagnose ist: "].
-prolog:message(unable_to_diagnose_broken_component(de)) --> ["Ich kann nicht herausfinden, welche Komponente kaputt ist. ",
-    "Bitte wenden Sie sich an einen Techniker.", nl].
+message_translation(diagnose_is(de),["Die Diagnose ist: "]).
+message_translation(unable_to_diagnose_broken_component(de),["Ich kann nicht herausfinden, welche Komponente kaputt ist. ",
+    "Bitte wenden Sie sich an einen Techniker."]).
--- src/translations_en.pl
+++ src/translations_en.pl
@@ -16,39 +16,42 @@
 
 % This file contain all the translations in American English. This language will be used when the current locale don't have any translations.
 % All these messages must be translated to the locales supported by the assistant.
+:- module(translations_en,
+	  [ message_translation/2]).
 
 :- multifile
-        prolog:message//1.
+        prolog:message//1, message_translation//2.
 
-prolog:message(battery_light_turns_on(en)) --> ["Is the battery light turned on after the power button is turned on?"].
-prolog:message(hard_disk_spins_on_a_second_motherboard(en)) --> ["Does the hard disk spin when connected to a second motherboard?"].
-prolog:message(known_working_hard_disk_do_not_spin_on_motherboard(en)) --> ["Does a hard disk which is known to work don't spin on the motherboard?"].
-prolog:message(ram_modules_not_compatible_with_motherboard(en)) --> ["The speed and form of the RAM modules are not documented on the manual of the motherboard?"].
-prolog:message(ram_modules_were_updated_lately(en)) --> ["Was the RAM module changed lately?"].
-prolog:message(screen_is_not_displaying_anything(en)) --> ["Is display blank? It doesn't display anything"].
-prolog:message(system_turns_on(en)) --> ["Does the system turn on?"].
-prolog:message(system_works_after_reinserting_power_cable_connector(en)) --> ["The system turns on after taking out and reinserting the power connector from the PSU to the motherboard, right?"].
-prolog:message(system_has_a_power_supply_unit(en)) --> ["Does the system have a power supply unit? Laptops don't have one, while desktops do."].
-prolog:message(system_works_after_reinserting_the_ram_modules(en)) --> ["After you remove the RAM module(s)" ,
-    " and insert them in the slot, does the system work?"].
-prolog:message(system_works_on_another_outlet(en)) --> ["Does the system work in another power outlet?"].
-prolog:message(system_works_with_a_new_motherboard(en)) --> ["Does the computer work with a new motherboard?"].
-prolog:message(system_works_with_a_new_power_supply_unit(en)) --> ["Does the computer work with a new power supply?"].
-prolog:message(system_works_with_another_power_cord(en)) --> ["Does the computer work with a another power cord?"].
-prolog:message(system_works_with_old_modules(en)) --> ["Does the computer work with the old RAM module(s)?"].
+:- discontiguous message_translation/2.
 
-prolog:message(no_electricity_on_the_power_outlet(en)) --> ["No electricity on the power outlet"].
-prolog:message(power_cord_is_broken(en)) --> ["The power cord is broken"].
-prolog:message(motherboard_power_cable_is_disconnected(en)) --> ["The cable which gives electricity to the motherboard was disconnected"].
-prolog:message(motherboard_is_burnt(en)) --> ["The motherboard is burnt"].
-prolog:message(power_supply_unit_is_burnt(en)) --> ["The power supply unit is burnt"].
-prolog:message(incompatible_ram_modules(en)) --> ["The RAM module is incompatible with the motherboard"].
-prolog:message(disconnected_ram_modules(en)) --> ["The RAM module was disconnected from the motherboard"].
+message_translation(battery_light_turns_on(en),["Is the battery light turned on after the power button is turned on?"]).
+message_translation(hard_disk_spins_on_a_second_motherboard(en),["Does the hard disk spin when connected to a second motherboard?"]).
+message_translation(known_working_hard_disk_do_not_spin_on_motherboard(en),["Does a hard disk which is known to work don't spin on the motherboard?"]).
+message_translation(ram_modules_not_compatible_with_motherboard(en),["The speed and form of the RAM modules are not documented on the manual of the motherboard?"]).
+message_translation(ram_modules_were_updated_lately(en),["Was the RAM module changed lately?"]).
+message_translation(screen_is_not_displaying_anything(en),["Is display blank? It doesn't display anything"]).
+message_translation(system_turns_on(en),["Does the system turn on?"]).
+message_translation(system_works_after_reinserting_power_cable_connector(en),["The system turns on after taking out and reinserting the power connector from the PSU to the motherboard, right?"]).
+message_translation(system_has_a_power_supply_unit(en),["Does the system have a power supply unit? Laptops don't have one, while desktops do."]).
+message_translation(system_works_after_reinserting_the_ram_modules(en),["After you remove the RAM module(s)" ,
+    " and insert them in the slot, does the system work?"]).
+message_translation(system_works_on_another_outlet(en),["Does the system work in another power outlet?"]).
+message_translation(system_works_with_a_new_motherboard(en),["Does the computer work with a new motherboard?"]).
+message_translation(system_works_with_a_new_power_supply_unit(en),["Does the computer work with a new power supply?"]).
+message_translation(system_works_with_another_power_cord(en),["Does the computer work with a another power cord?"]).
+message_translation(system_works_with_old_modules(en),["Does the computer work with the old RAM module(s)?"]).
+
+message_translation(no_electricity_on_the_power_outlet(en),["No electricity on the power outlet"]).
+message_translation(power_cord_is_broken(en),["The power cord is broken"]).
+message_translation(motherboard_power_cable_is_disconnected(en),["The cable which gives electricity to the motherboard was disconnected"]).
+message_translation(motherboard_is_burnt(en),["The motherboard is burnt"]).
+message_translation(power_supply_unit_is_burnt(en),["The power supply unit is burnt"]).
+message_translation(incompatible_ram_modules(en),["The RAM module is incompatible with the motherboard"]).
+message_translation(disconnected_ram_modules(en),["The RAM module was disconnected from the motherboard"]).
 
 prolog:message(questions_answered_yes(en)) --> ["=== Questions answered with yes === ", nl].
 prolog:message(questions_answered_no(en)) --> ["=== Questions answered with no === ", nl].
 prolog:message(questions_answered_report_end(en)) --> ["=== End of report ===", nl].
 
-prolog:message(answer_keys_list(en)) --> [" (yes/y/no/n) "].
-prolog:message(diagnose_is(en)) --> ["The issue is: "].
-prolog:message(unable_to_diagnose_broken_component(en)) --> ["I can not diagnose the broken component. Please contact a technician.", nl].
+message_translation(diagnose_is(en),["The issue is: "]).
+message_translation(unable_to_diagnose_broken_component(en),["I can not diagnose the broken component. Please contact a technician."]).
