368c84de3af2818b8d8129917610c9e52c8c6022
Antonio Robirosa <19760446+arobirosa@users.noreply.github.com>
2024-02-08 17:23:38 +0100

    8 Now the language parameter is parsed

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env swipl
 %
-%   Copyright 2024 Antonio Robirosa <expert.system@go.areko.consulting>
+%   Computer Repair AI Assistant using Prolog
+%
+%   Author:        Antonio Robirosa
+%   E-mail:        expert.system@go.areko.consulting
+%   Website:       https://areko.consulting
+%   Copyright 2024, Antonio Robirosa
 %
 %   Licensed under the Apache License, Version 2.0 (the "License");
 %   you may not use this file except in compliance with the License.
@@ -14,13 +19,20 @@
 %   See the License for the specific language governing permissions and
 %   limitations under the License.
 %
-
 % This PrologScrit process the arguments and starts the assistent.
 :- initialization(main, main).
 :- consult('src/computer_repair_assistant.pl').
 
+opt_help(help(header), "Computer Repair AI Assistant v1.0.0").
+opt_help(help(footer), [ansi(bold, '\n~w', ["Please start this script without parameters to start the diagnose of your computer on an browser."])]).
+opt_help(language,"Switches the language of the application. If none is given, all questions are shown in English (en)").
+
+opt_type(language, language, oneof([en, de, es])).
+
 main(Argv) :-
-    atomic_list_concat(Argv, ' ', SingleArg),
-    term_to_atom(Term, SingleArg),
-    Val is Term,
-    format('~w~n', [Val]).
+    % Parse the command-line arguments
+    argv_options(Argv, _PositionalArgs, ReceivedArguments),
+    option(language(Language), ReceivedArguments, en),
+    format('Locale set to: ~w~n', [Language]),
+    halt.
+
