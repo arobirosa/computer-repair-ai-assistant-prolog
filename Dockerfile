bc13a9e2b1a5b31d70d3b4f39981212963fc1b66
Antonio Robirosa <19760446+arobirosa@users.noreply.github.com>
2024-02-19 17:54:05 +0100

    2 Now a docker image can be built. Not working

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM swipl:stable
+COPY . .
+CMD ["swipl", "docker_start.pl"]
+LABEL org.opencontainers.image.source=https://github.com/arobirosa/computer-repair-ai-assistant-prolog
+LABEL org.opencontainers.image.description="AI Computer Assistant. Proof of concept to evaluate the use of knowledge representation and reasoning (KRR) systems "
+LABEL org.opencontainers.image.licenses=Apache-2.0
---
+++ docker_start.pl
@@ -0,0 +1,29 @@
+%
+%   Computer Repair AI Assistant using Prolog
+%
+%   Author:        Antonio Robirosa
+%   E-mail:        expert.system@go.areko.consulting
+%   Website:       https://areko.consulting
+%   Copyright 2024, Antonio Robirosa
+%
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%     http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+%
+% This prolog script is the entry point to for the docker image
+:- initialization(main, main).
+:- consult('src/computer_repair_assistant.pl').
+
+main :-
+    areko_localization:store_locale(en),
+    start_server,
+    start.
+
